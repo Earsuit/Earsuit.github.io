36b45d98acda5a683be9fb0a405363f131d3b1ac
Yudi <yudi.ren@synsense.ai>
2022-04-16 22:40:43 +0800

    update format

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'XIANYU'
-copyright = '2022, Earsuit renyudicn@ouotlook.com'
+copyright = '2022, Earsuit'
 author = 'Earsuit'
 
 # -- General configuration ---------------------------------------------------
@@ -56,10 +56,47 @@ html_static_path = ['_static']
 fontawesome_included = True
 
 html_sidebars = {
-   '**': ['searchbox.html', 
-          'postcard.html', 
-           'recentposts.html',
-          'tagcloud.html', 
-          'categories.html',
-          'archives.html', ]
+    "index": ["aboutme.html", "archives.html"],
+    "about": ["aboutme.html"],
+    "blog": ["tagcloud.html", "archives.html"],
+    "blog/**": ["postcard.html", "archives.html"],
 }
+
+html_theme_options = {
+    # If you want to configure Twitter or Github social media buttons to show up to the right of your nav bar,
+    # you can use the "github_url" and "twitter_url" options:
+    "github_url": "https://github.com/Earsuit/",
+    # You can also change the text that is in the search bar before people click on it by setting the
+    # "search_bar_text"
+    "search_bar_text": "Search for treasure...",
+    # By default your site will have a search bar in the nav bar, but when we include the about.html,
+    # this gets removed to so you can add one to the top "navbar" instead
+    "navbar_end": ["navbar-icon-links.html", "search-field.html"]
+}
+
+def setup(app):
+    app.add_css_file("custom.css")
+
+   
+# If you used the blog file path in part one you do not need to set this value, however if you change it,
+# this should be set to the same value.
+# blog_path = "posts"
+
+# blog_feed_fulltext: Choose to display full text in blog feeds.
+blog_feed_fulltext = True
+
+# Glob pattern that grabs all posts so you don't need to specify which posts are blog posts in each post
+# This pattern facilitates a folder structure such as posts/2020/my-awesome-post.rst
+blog_post_pattern = "posts/*/*"
+
+# post_redirect_refresh: Number of seconds (default is 5) that a redirect page waits before refreshing the page
+# to redirect to the post.
+post_redirect_refresh = 1
+
+# post_auto_image: Index of the image that will be displayed in the excerpt of the post. Default is 0, meaning no
+# image. Setting this to 1 will include the first image
+post_auto_image = 0
+
+# post_auto_excerpt: Number of paragraphs (default is 1) that will be displayed as an excerpt from the post. Setting
+# this 0 will result in displaying no post excerpt in archive pages.
+post_auto_excerpt = 1
