1c7fc86d21c0a00633d07292f6b70e11a6f69a84
Yudi <yudi.ren@synsense.ai>
2022-04-16 21:01:20 +0800

    remove version

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -21,10 +21,6 @@ project = 'XIANYU'
 copyright = '2022, Earsuit'
 author = 'Earsuit'
 
-# The full version, including alpha/beta/rc tags
-release = '1.0'
-
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
