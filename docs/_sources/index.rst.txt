a6088bc68458520247befd9c181d8810aebcddd2
Yudi <yudi.ren@synsense.ai>
2022-04-16 16:40:25 +0800

    use sphinx-quickstart to generate

--- docs/_sources/index.rst.txt
+++ docs/_sources/index.rst.txt
@@ -1,2 +1,20 @@
-Welecome to XIANYU
-================================================
+.. XIANYU documentation master file, created by
+   sphinx-quickstart on Sat Apr 16 16:35:13 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to XIANYU's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- docsrc/conf.py
+++ docsrc/conf.py
@@ -1,10 +1,8 @@
-from pathlib import Path
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
@@ -20,18 +18,20 @@ from pathlib import Path
 # -- Project information -----------------------------------------------------
 
 project = 'XIANYU'
-author = 'SynSense'
+copyright = '2022, Earsuit'
+author = 'Earsuit'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0'
 
-master_doc = 'index'
+
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme"]
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -39,23 +39,15 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
-rst_prolog = """
-.. |tm|    unicode:: U+02122 .. TM sign
-   :trim:
-"""
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-
-html_css_files = [
-	'custom.css',
-]
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
